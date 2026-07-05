a9fac00c6b80ee819d4a1b8027d56ca7d69de419
KAUTARUMA <75647306+KAUTARUMA@users.noreply.github.com>
2026-07-04 23:42:42 -0400

    add scaffolding to sourcemapIgnoreList

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+	server: {
+		sourcemapIgnoreList: (sourcePath) => sourcePath.includes('scaffolding'),
+	},
 	plugins: [
 		sveltekit({
 			compilerOptions: {
